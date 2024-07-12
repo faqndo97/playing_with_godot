ee2f999a98bd3b47615db9237c5f5c12e4217d17
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2024-07-11 23:49:56 -0300

    Refactor player

--- 2d-project-start/player.gd
+++ 2d-project-start/player.gd
@@ -2,16 +2,27 @@ extends CharacterBody2D
 
 signal health_depleted
 
-var health = 100.0
 const DAMAGE_RATE = 15.0
+const MOVEMENT_DISTANCE = 600
 
+var health = 100.0
+var overlapping_mobs
+
+# Hooks
 func _physics_process(delta):
 	# * We have access to Input in all scripts we create
 	# * It's a classed provided by godot with several helpers to interact with user input
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 600 # Move in input direction at 600px by second
+	velocity = direction * MOVEMENT_DISTANCE # Move in input direction at MOVEMENT_DISTANCE by second
 	move_and_slide()
-	
+	animateBaseOnvelocity(velocity)
+
+	if hasOverlappingMobs():
+		make_damage(delta)
+
+# Private
+
+func animateBaseOnvelocity(velocity):
 	if velocity.length() > 0.0:
 		# $HappyBoo is the same as get_node("HappyBoo")
 		# The name in get_node needs to respect the folder structure unless node is marked as "Access as unique name"
@@ -20,15 +31,21 @@ func _physics_process(delta):
 		$HappyBoo.play_walk_animation()
 	else:
 		$HappyBoo.play_idle_animation()
-	
-	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 
-	if overlapping_mobs.size() > 0:
-		make_damage(delta, overlapping_mobs)
+func hasOverlappingMobs():
+	overlapping_mobs = %HurtBox.get_overlapping_bodies()
 
-func make_damage(delta, overlapping_mobs):
+	return overlapping_mobs.size() > 0
+
+func make_damage(delta):
 	health -= DAMAGE_RATE * overlapping_mobs.size() * delta
-	%HealthBar.value = health
+	updateHealthBar()
 	
-	if health <= 0.0:
+	if isHealthDepleted():
 		health_depleted.emit()
+
+func updateHealthBar():
+	%HealthBar.value = health
+
+func isHealthDepleted():
+	return health <= 0.0
